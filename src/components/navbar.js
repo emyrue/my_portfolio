7b7b38f8f4fe1d5709d3184c2b2a6d2ab878fd64
emyrue <94935633+emyrue@users.noreply.github.com>
2022-09-26 16:29:25 -0600

    Styled navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,24 @@
+import '../style/navbar.css';
+
+function NavBar() {
+  return (
+    <nav>
+      <ul className="nav-links">
+        <li>
+          <a className="nav-link" href="#">Home</a>
+        </li>
+        <li>
+          <a className="nav-link" href="#projects">Projects</a>
+        </li>
+        <li>
+          <a className="nav-link" href="#about">About</a>
+        </li>
+        <li>
+          <a className="nav-link" href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
