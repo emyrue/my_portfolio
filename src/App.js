8e63e58fd3aa8b335de5b617ee6812ee08ea6e19
emyrue <94935633+emyrue@users.noreply.github.com>
2022-09-27 13:51:52 -0600

    Add headings and media queries

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NavBar from './components/navbar';
+import HomePage from './pages/HomePage';
 import './App.css';
 
 function App() {
   return (
     <div className="app-container">
       <NavBar />
+      <HomePage />
     </div>
   );
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import '../style/homepage.css';
+
+function HomePage() {
+  return (
+    <section>
+      <div className="homepage-header">
+        <h1>Emily Robertson</h1>
+      </div>
+    </section>
+  );
+}
+
+export default HomePage;
