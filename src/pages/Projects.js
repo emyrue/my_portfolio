2e1c66a323e4ff383047569cf133580d7776af7b
emyrue <94935633+emyrue@users.noreply.github.com>
2022-12-12 17:03:33 -0700

    Elements for single project added

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,6 +6,7 @@ function Projects() {
       class_names: "project project-one",
       class_names_2: "project-info project-info-one",
       class_names_3: "project-image project-image-one",
+      class_names_4: "project-blue project-blue-one",
       github_link: "",
       description: "My first project",
       screenshot: "",
@@ -17,13 +18,17 @@ function Projects() {
       {allProjects.map((project) => {
         return (
           <div className={project.class_names}>
-            <div className={project.class_names_2}>
-              <h3 className="project-title">{project.title}</h3>
-              <ul className="project-technologies">
-                {project.technologies.map((technology) => {
-                  return (<li>{technology}</li>);
-                })}
-              </ul>
+            <img className={project.class_names_3} src={project.screenshot} />
+            <div className={project.class_names_4}>
+              <div className={project.class_names_2}>
+                <h3 className="project-title">{project.title}</h3>
+                <ul className="project-technologies">
+                  {project.technologies.map((technology) => {
+                    return (<li>{technology}</li>);
+                  })}
+                </ul>
+              </div>
+              <button>See this project</button>
             </div>
           </div>
         );
