05c2e49fdeb794e28750a337b76559e1a1f272b2
emyrue <94935633+emyrue@users.noreply.github.com>
2022-12-14 17:59:01 -0700

    Created mobile nav section

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,23 +1,43 @@
 import '../style/navbar.css';
+import { useState } from 'react';
 
 function NavBar() {
+  const [classes, setClasses] = useState("mobile-nav");
   return (
-    <nav className="desktop-nav">
-      <ul className="nav-links">
-        <li>
-          <a className="nav-link" href="/">Home</a>
-        </li>
-        <li>
-          <a className="nav-link" href="#projects">Projects</a>
-        </li>
-        <li>
-          <a className="nav-link" href="#about">About</a>
-        </li>
-        <li>
-          <a className="nav-link" href="#contact">Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <nav className="desktop-nav">
+       <ul className="nav-links">
+          <li>
+            <a className="nav-link" href="/">Home</a>
+          </li>
+          <li>
+            <a className="nav-link" href="#projects">Projects</a>
+          </li>
+          <li>
+            <a className="nav-link" href="#about">About</a>
+          </li>
+          <li>
+            <a className="nav-link" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <div className={classes}>
+        <ul className="mobile-nav-links">
+          <li>
+            <a className="mobile-nav-link" href="/">Home</a>
+          </li>
+          <li>
+            <a className="mobile-nav-link" href="#projects">Projects</a>
+          </li>
+          <li>
+            <a className="mobile-nav-link" href="#about">About</a>
+          </li>
+          <li>
+            <a className="mobile-nav-link" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
