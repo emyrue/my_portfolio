d963295da33cb1b8704dac77ede28204757a60c6
emyrue <94935633+emyrue@users.noreply.github.com>
2023-01-31 10:27:41 -0700

    Make popup take up whole screen

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import closePopup from '../images/close.svg';
 import '../style/projectpopup.css';
 
 function ProjectPopup(props) {
   const { array, index, show, closeProject } = props;
   const [projectIndex, setProjectIndex] = useState(index);
+
+  useEffect(() => {
+    setProjectIndex(index);
+  }, [show])
+
   return (
     <section className={show ? "project-popup":"hide-project"} >
-      {index}
+      <img className="popup-screenshot" alt="screenshot" src={array[projectIndex].screenshot} />
       <button onClick={() => {closeProject()}}>
         <img src={closePopup} alt="close" className="close-popup" />
       </button>
