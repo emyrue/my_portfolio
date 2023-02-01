607bd8f2b90e12ea27bf3423d39a423b471cf641
emyrue <94935633+emyrue@users.noreply.github.com>
2023-02-01 15:56:38 -0700

    Fix font families

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,18 @@
+import download from '../images/download.svg';
 import '../style/about.css';
 
 function About() {
   return (
-    <section className="about-container">
+    <section id="about">
       <div className="about-content">
         <h3 className="about-heading">About</h3>
         <p className="about-text">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ea eiusmod
         </p>
-        <button className="cv-button">Get my CV</button>
+        <button className="cv-button">
+          <span>Get my CV</span>
+          <img className="about-download" src={download} alt="" />
+        </button>
       </div>
     </section>
   );
