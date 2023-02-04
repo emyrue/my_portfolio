dd31d112eff57992ab67fc0abee4a96883217af1
emyrue <94935633+emyrue@users.noreply.github.com>
2023-02-03 17:35:44 -0700

    Style mobile form text and change z-index of menu icon

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,19 +7,19 @@ function Contact() {
   return (
     <section id="contact">
       <form>
-        <h2 className="form-heading">Get Started</h2>
+        <h2 className="form-heading">Get started</h2>
         <p className="form-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         <div className="flex-container">
           <div className="input-div">
-            <label htmlFor="name">Your Name</label>
+            <label className="form-label" htmlFor="name">Your Name</label>
             <input className="input-box" type="text" id="name" name="name" />
           </div>
           <div className="input-div">
-            <label htmlFor="email">Your Email</label>
+            <label className="form-label" htmlFor="email">Your Email</label>
             <input className="input-box" type="text" id="email" name="email" />
           </div>
         </div>
-        <textarea name="message" id="message" cols="30" rows="10" placeholder="Write your message"></textarea>
+        <textarea className="text-area" name="message" id="message" cols="30" rows="10" placeholder="Write your message"></textarea>
         <button type="submit" className="submit-button">Start Collaboration</button>
       </form>
       <img src={mobileTop} alt="" className="mobile-top" />
