8854decdccf372f171058ee29a80880dfd75b692
emyrue <94935633+emyrue@users.noreply.github.com>
2023-04-14 18:02:33 -0600

    Added formspree form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,7 +6,7 @@ import '../style/contact.css';
 function Contact() {
   return (
     <section id="contact">
-      <form>
+      <form action="https://formspree.io/f/mjvlwyao" method="POST">
         <h2 className="form-heading">Get started</h2>
         <p className="form-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         <div className="flex-container">
